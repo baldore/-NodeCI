f23304bb34f590c532b846a363cf5e3644cddeca
aorrego <aorrego@hugeinc.com>
2020-03-22 18:54:45 -0500

    Add test with login

---
+++ tests/header.test.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false })
+  page = await browser.newPage()
+  await page.goto('localhost:3000')
+})
+
+afterEach(async () => {
+  // await browser.close()
+})
+
+test('the header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+  expect(text).toEqual('Blogster')
+})
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a')
+  const url = await page.url()
+
+  expect(url).toMatch(/accounts\.google\.com/)
+})
+
+test.only('when sign in, shows logout button', async () => {
+  const logoutSelector = 'a[href="/auth/logout"]'
+  const id = '5e41c86aeddec42016cc29b0'
+
+  const Buffer = require('safe-buffer').Buffer
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  }
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64',
+  )
+
+  const Keygrip = require('keygrip')
+  const keys = require('../config/keys')
+  const keygrip = new Keygrip([keys.cookieKey])
+  const sig = keygrip.sign('session=' + sessionString)
+
+  await page.setCookie({ name: 'session', value: sessionString })
+  await page.setCookie({ name: 'session.sig', value: sig })
+  await page.goto('localhost:3000')
+  await page.waitFor(logoutSelector)
+
+  const text = await page.$eval(logoutSelector, el => el.innerHTML)
+  expect(text).toEqual('Logout')
+})
